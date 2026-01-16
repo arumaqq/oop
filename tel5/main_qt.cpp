c5c8593881cbd12005f7698e6491a1cb0d7a6466
arumaqq <carevp61@gmail.com>
2026-01-16 08:17:03 +0300

    main_qt.cpp

--- tel5/main_qt.cpp
+++ tel5/main_qt.cpp
@@ -9,12 +9,10 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     
-    // Set UTF-8 encoding for Russian language support
     #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
     #endif
     
-    // Set Russian locale
     QLocale::setDefault(QLocale(QLocale::Russian, QLocale::Russia));
     
     MainWindow window;
